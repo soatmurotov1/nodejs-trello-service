55f55fd58648a94767f62688028b4af04a5e3fd0
soatmurotov1 <soatmurotovabrorbek23@gmail.com>
2025-10-09 03:02:45 +0500

    controller ichidagi boardni tugatdim.

---
+++ config/database.js
@@ -0,0 +1,13 @@
+import { Pool } from "pg";
+
+
+const clint = {
+    user: "postgres",
+    host: "localhost",
+    password: "root",
+    database: "node_trello_service",
+    port: 5432
+}
+
+
+export default clint
